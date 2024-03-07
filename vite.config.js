7fda284dc2d68348b2a43d5a29a83392d56f77a5
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-03-07 23:27:53 +0800

    feat!: release sl initial content

---
+++ vite.config.js
@@ -0,0 +1,31 @@
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+import react from "@vitejs/plugin-react-swc";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Sassy Library",
+        short_name: "SL",
+        description: "A simple CSS library using Sass",
+        theme_color: "#FFFFFF",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
