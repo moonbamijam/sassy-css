230e37f39409ac0fa2de0f71ee92f7266beae82f
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-08-20 19:51:25 +0800

    feat: responsive header (#28)

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,15 @@
+import { Menu } from "lucide-react";
 import { navLinks } from "../../lib/navLinks";
 import Styles from "./header.module.scss";
+import { useState } from "react";
 
 const Header = () => {
+  const [dropdown, setDropdown] = useState(true);
+
+  const toggleDropdown = () => {
+    setDropdown(!dropdown)
+  }
+
   return (
     <>
       <div className={`${Styles.header_bg}`}></div>
@@ -9,13 +17,19 @@ const Header = () => {
         <a href="/" className={`${Styles.title}`}>
           <h2>Sassy CSS</h2>
         </a>
-        <nav className={`${Styles.nav}`}>
-          {navLinks.map((navLink, id) => (
-            <a key={id} href={navLink.href} className={`${Styles.a}`}>
-              {navLink.name}
-            </a>
-          ))}
-        </nav>
+
+        <button className={`btn ${Styles.btn}`} onClick={toggleDropdown}>
+          <Menu />
+          {dropdown && (
+            <nav className={`${Styles.nav}`}>
+              {navLinks.map((navLink, id) => (
+                <a key={id} href={navLink.href} className={`${Styles.a}`}>
+                  {navLink.name}
+                </a>
+              ))}
+            </nav>
+          )}
+        </button>
       </header>
     </>
   );
